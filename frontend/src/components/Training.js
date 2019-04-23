834a51e87af48a218edf8d8280a8c76ed043a1a5
Jon Gear <askjon@jongear.io>
2019-04-22 21:53:15 -0500

    update to brain emoji

--- frontend/src/components/Training.js
+++ frontend/src/components/Training.js
@@ -149,29 +149,29 @@ export default class FlightControls extends React.Component {
               >
                 <Step transition="scale">
                   {({ accomplished }) => (
-                    <img
+                    <span
                       style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                      width="30"
-                      src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
-                    />
+                    >
+                      🧠
+                    </span>
                   )}
                 </Step>
                 <Step transition="scale">
                   {({ accomplished }) => (
-                    <img
+                    <span
                       style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                      width="30"
-                      src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
-                    />
+                    >
+                      🧠
+                    </span>
                   )}
                 </Step>
                 <Step transition="scale">
                   {({ accomplished }) => (
-                    <img
+                    <span
                       style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
-                      width="30"
-                      src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
-                    />
+                    >
+                      🧠
+                    </span>
                   )}
                 </Step>
               </ProgressBar>
